6554642d5453041af4dc1aab0fe1be4950a3871a
Alexandre Ferreira <almcfx@gmail.com>
2018-05-26 22:42:28 -0300

    Projeto criado

---
+++ src/main/java/com/ax37tech/geolocationapi/GeolocationapiApplication.java
@@ -0,0 +1,12 @@
+package com.ax37tech.geolocationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ax37tech/geolocationapi/GeolocationapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ax37tech.geolocationapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeolocationapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
